26e47cf2d285b94aa6864f937751a018becad6d1
Omer <om.yizhaq@gmail.com>
2020-06-21 15:53:37 +0300

    Change of the way hsp find works
    previously dict was made from the db only
    now dict is formed for 2 sequences (both are long enpugh)
    
    + names were changes from query and db to seq1 and seq2

--- hsp_handle.py
+++ hsp_handle.py
@@ -1,20 +1,26 @@
 from copy import copy
+import utils
+
+ALPHABET = 'ARNDCQEGHILKMFPSTWYVBZX*'
+# NOF:
+# I changes a few names here so it makes more sense (db and query was less relevant)
+# the functions here may be changed so that the build of the dict will only happen once (it takes too long)
 
 class HSP():
-    """Represents an alignment with score >=T between a kmer in the query and a kmer in the db"""
+    """Represents an alignment with score >=T between a kmer in the seq1 and a kmer in the seq2"""
 
-    def __init__(self, query_start, query_end, db_start, db_end, score):
-        self.query_start = query_start
-        self.query_end = query_end
-        self.db_start = db_start
-        self.db_end = db_end
+    def __init__(self, seq1_start, seq1_end, seq2_start, seq2_end, score):
+        self.seq1_start = seq1_start
+        self.seq1_end = seq1_end
+        self.seq2_start = seq2_start
+        self.seq2_end = seq2_end
         self.score = score
 
     def __eq__(self, other):
         if isinstance(other, self.__class__):
-            is_query_eq = self.query_start == other.query_start and self.query_end == other.query_end
-            is_db_eq = self.db_start == other.db_start and self.db_end == other.db_end
-            return is_query_eq and is_db_eq and self.score == other.score
+            is_seq1_eq = self.seq1_start == other.seq1_start and self.seq1_end == other.seq1_end
+            is_seq2_eq = self.seq2_start == other.seq2_start and self.seq2_end == other.seq2_end
+            return is_seq1_eq and is_seq2_eq and self.score == other.score
         return False
 
     def __ne__(self, other):
@@ -22,84 +28,87 @@ class HSP():
         return not self.__eq__(other)
 
     def __hash__(self):
-        return hash(str((self.query_start, self.query_end, self.db_start, self.db_end, self.score)))
+        return hash(str((self.seq1_start, self.seq1_end, self.seq2_start, self.seq2_end, self.score)))
 
     def __str__(self):
-        return f'''
-                Score: {self.score}
-                Query: [{self.query_start}, {self.query_end}]
-                DB: [{self.db_start}, {self.db_end}]
+        return f''' 
+                Score: {self.score} 
+                Query: [{self.seq1_start}, {self.seq1_end}] 
+                DB: [{self.seq2_start}, {self.seq2_end}] 
                 '''
 
     def __repr__(self):
         return self.__str__()
 
 
-def get_hsps(query, db_dict, k, scoring_matrix, T):
-    hsps = []
-    query_dict = build_db(query, k)
 
-    for key, val in query_dict.items():
-        neighbors = find_neighbors(key, scoring_matrix, ALPHABET, T)
+def get_hsps(seq1, seq2, k, scoring_matrix, T):
+    hsps = []
+    seq1_dict = utils.map_sequence(seq1, k)
+    seq2_dict = utils.map_sequence(seq2, k)
+
+    for key, val in seq1_dict.items():
+        neighbors = utils.find_neighbors(key, scoring_matrix, ALPHABET, T)
         for neighbor in neighbors:
-            if neighbor in db_dict.keys():
-                score = align(key, neighbor, scoring_matrix)
-                for query_start in val:
-                    query_end = query_start + k - 1
-                    for db_start in db_dict[neighbor]:
-                        db_end = db_start + k - 1
-                        hsp = HSP(query_start, query_end, db_start, db_end, score)
+            if neighbor in seq2_dict.keys():
+                score = utils.align(key, neighbor, scoring_matrix)
+                for seq1_start in val:
+                    seq1_end = seq1_start + k - 1
+                    for seq2_start in seq2_dict[neighbor]:
+                        seq2_end = seq2_start + k - 1
+                        hsp = HSP(seq1_start, seq1_end, seq2_start, seq2_end, score)
                         hsps.append(hsp)
 
     return hsps
 
 
-def extend_left(query, db, hsp, scoring_matrix, X):
+def extend_left(seq1, seq2, hsp, scoring_matrix, X):
     """returns the left extension with the maximal score"""
 
     msp = copy(hsp)
 
-    while (hsp.score > msp.score - X and hsp.query_start > 0 and hsp.db_start > 0):
-        hsp.query_start = hsp.query_start - 1
-        hsp.db_start = hsp.db_start - 1
-        hsp.score = hsp.score + scoring_matrix[query[hsp.query_start], db[hsp.db_start]]
+    while (hsp.score > msp.score - X and hsp.seq1_start > 0 and hsp.seq2_start > 0):
+        hsp.seq1_start = hsp.seq1_start - 1
+        hsp.seq2_start = hsp.seq2_start - 1
+        hsp.score = hsp.score + scoring_matrix[seq1[hsp.seq1_start], seq2[hsp.seq2_start]]
         if (hsp.score > msp.score):
             msp = copy(hsp)
 
     return msp
 
 
-def extend_right(query, db, hsp, scoring_matrix, X):
+def extend_right(seq1, seq2, hsp, scoring_matrix, X):
     """returns the right extension with the maximal score"""
 
     msp = copy(hsp)
 
-    while (hsp.score > msp.score - X and hsp.query_end < len(query) - 1 and hsp.db_end < len(db) - 1):
-        hsp.query_end = hsp.query_end + 1
-        hsp.db_end = hsp.db_end + 1
-        hsp.score = hsp.score + scoring_matrix[query[hsp.query_end], db[hsp.db_end]]
+    while (hsp.score > msp.score - X and hsp.seq1_end < len(seq1) - 1 and hsp.seq2_end < len(seq2) - 1):
+        hsp.seq1_end = hsp.seq1_end + 1
+        hsp.seq2_end = hsp.seq2_end + 1
+        hsp.score = hsp.score + scoring_matrix[seq1[hsp.seq1_end], seq2[hsp.seq2_end]]
         if (hsp.score > msp.score):
             msp = copy(hsp)
 
     return msp
 
 
-def extend_hsp(query, db, hsp, scoring_matrix, X):
-    msp_left = extend_left(query, db, hsp, scoring_matrix, X)
-    msp = extend_right(query, db, msp_left, scoring_matrix, X)
+def extend_hsp(seq1, seq2, hsp, scoring_matrix, X):
+    msp_left = extend_left(seq1, seq2, hsp, scoring_matrix, X)
+    msp = extend_right(seq1, seq2, msp_left, scoring_matrix, X)
 
     return msp
 
 
-def find_msps(query, db, k, scoring_matrix, T, X):
+def find_msps(seq1, seq2, k, scoring_matrix, T, X):
     msps = set()
+    hsps = get_hsps(seq1, seq2, k, scoring_matrix, T)
 
-    db_dict = build_db(db, k)
-    hsps = get_hsps(query, db_dict, k, scoring_matrix, T)
-
+# there is a need to add counter (according to the assignment)
     for hsp in hsps:
-        msp = extend_hsp(query, db, hsp, scoring_matrix, X)
+        msp = extend_hsp(seq1, seq2, hsp, scoring_matrix, X)
         msps.add(msp)
 
     return msps
+
+
 
